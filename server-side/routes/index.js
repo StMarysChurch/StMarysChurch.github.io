b48c1782132e454d0142eecd7acf3bb48dd97646
RoneyThomas <roneythomas6@gmail.com>
2016-08-30 01:57:55 -0400

    Initial support for handebar template

--- server-side/routes/index.js
+++ server-side/routes/index.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var router = express.Router();
+var firebase = require("firebase");
+var handlebars = require("handlebars");
+
+firebase.initializeApp({
+    serviceAccount: "../Church Tools-57d50b5b341e.json",
+    databaseURL: "https://church-tools.firebaseio.com"
+});
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    // res.render('index', { title: 'Express'});
+    var db = firebase.database();
+    var ref = db.ref("schedule/");
+    ref.once("value").then(function (snapshot) {
+        // res.render('index', {schedule: snapshot.val(), layout: null});
+        res.render('index', {schedule: snapshot.val(), layout: null}, function (err, html) {
+            res.send(html);
+            console.log(html)
+        });
+        //console.log(res.render('index', {schedule: snapshot.val(), layout: null}));
+        // snapshot.forEach(function (childSnapshot) {
+        //     var key = childSnapshot.key;
+        //     var childData = childSnapshot.val();
+        //     console.log(key);
+        //     console.log(childData);
+        // });
+    });
+    //res.render('index', {schedule: snapshot.val(), layout: null});
 });
 
 module.exports = router;

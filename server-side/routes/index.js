b9f9ee8310de925eae8f6a44caa9f6a942ad3543
RoneyThomas <roneythomas6@gmail.com>
2016-08-30 23:47:26 -0400

    Removed index.html as it is generated from template.

--- server-side/routes/index.js
+++ server-side/routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var firebase = require("firebase");
 var handlebars = require("handlebars");
+const fs = require('fs');
 
 firebase.initializeApp({
     serviceAccount: "../Church Tools-57d50b5b341e.json",
@@ -13,21 +14,18 @@ router.get('/', function (req, res, next) {
     // res.render('index', { title: 'Express'});
     var db = firebase.database();
     var ref = db.ref("schedule/");
-    ref.once("value").then(function (snapshot) {
-        // res.render('index', {schedule: snapshot.val(), layout: null});
-        res.render('index', {schedule: snapshot.val(), layout: null}, function (err, html) {
-            res.send(html);
-            console.log(html)
+    ref.once("value").then((snapshot) => {
+        fs.readFile("../views/index.hbs", "utf8", (err, data) => {
+            if (err) throw err;
+            var template = handlebars.compile(data);
+            var response = template(snapshot.val());
+            res.send(response);
+            fs.writeFile('../../public/index.html', template(snapshot.val()), (err) => {
+                if (err) throw err;
+                console.log('It\'s saved!');
+            });
         });
-        //console.log(res.render('index', {schedule: snapshot.val(), layout: null}));
-        // snapshot.forEach(function (childSnapshot) {
-        //     var key = childSnapshot.key;
-        //     var childData = childSnapshot.val();
-        //     console.log(key);
-        //     console.log(childData);
-        // });
     });
-    //res.render('index', {schedule: snapshot.val(), layout: null});
 });
 
 module.exports = router;

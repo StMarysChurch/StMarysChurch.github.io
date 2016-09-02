7ab85873e365d34b74de5c1c8b661dbe9e8c8f8d
RoneyThomas <roneythomas6@gmail.com>
2016-09-01 21:29:49 -0400

    Auto deploy from managed VM

--- server-side/app.js
+++ server-side/app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+//var gcs = require("@google-cloud/storage")();
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -14,11 +15,11 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// uncomment after placing your favicon in /deploy
+//app.use(favicon(path.join(__dirname, 'deploy', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -26,10 +27,10 @@ app.use('/', routes);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use((req, res, next) => {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handlers
@@ -37,24 +38,43 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
     });
-  });
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
 
+// Activate Google Cloud Trace and Debug when in production
+if (process.env.NODE_ENV === 'production') {
+    require('@google/cloud-trace').start();
+    require('@google/cloud-debug');
+}
+
+// var secrets = gcs.bucket("church-tools.appspot.com");
+// var firebaseKey = secrets.bucket("secret/church-tools-key.json");
+// var deployKey = secrets.bucket("secret/deploy-token.json");
+// firebaseKey.download({
+//     destination: path.resolve(__dirname, "../church-tools-key.json")
+// }, function (err) {
+// });
+//
+// deployKey.download({
+//     destination: path.resolve(__dirname, "../deploy-token.json")
+//     /Users/roneythomas/code/StMarysChurch.github.io/server-side/deploy-token.json
+// }, function (err) {
+// });
 
 module.exports = app;
--- server-side/routes/index.js
+++ server-side/routes/index.js
@@ -1,31 +1,46 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var path = require("path");
 var firebase = require("firebase");
+var client = require("firebase-tools");
 var handlebars = require("handlebars");
-const fs = require('fs');
+var deployToken = require(path.resolve(__dirname, "../deploy-token.json"));
+const fs = require("fs");
 
 firebase.initializeApp({
-    serviceAccount: "../Church Tools-57d50b5b341e.json",
+    serviceAccount: path.resolve(__dirname, "../church-tools-key.json"),
     databaseURL: "https://church-tools.firebaseio.com"
 });
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-    // res.render('index', { title: 'Express'});
+router.get("/", (req, res, next) => {
+    // res.render("index", { title: "Express"});
+    console.log(__dirname);
     var db = firebase.database();
     var ref = db.ref("schedule/");
     ref.once("value").then((snapshot) => {
-        fs.readFile("../views/index.hbs", "utf8", (err, data) => {
+        fs.readFile(path.resolve(__dirname, "../views/index.hbs"), "utf8", (err, data) => {
             if (err) throw err;
             var template = handlebars.compile(data);
             var response = template(snapshot.val());
             res.send(response);
-            fs.writeFile('../../public/index.html', template(snapshot.val()), (err) => {
+            fs.writeFile(path.resolve(__dirname, "../deploy/hosting/index.html"), template(snapshot.val()), (err) => {
                 if (err) throw err;
-                console.log('It\'s saved!');
+                console.log("It\"s saved!");
+                client.deploy({
+                    project: "church-tools",
+                    // token: process.env.FIREBASE_TOKEN,
+                    token: deployToken.token,
+                    cwd: path.resolve(__dirname, "../deploy/")
+                }).then(() => {
+                    console.log("Rules have been deployed!")
+                }).catch((err) => {
+                    // handle error
+                });
             });
         });
     });
+    next();
 });
 
 module.exports = router;

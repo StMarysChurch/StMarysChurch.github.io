2bdc937a936e6c6a19db5be213c7c57b2a32b18c
RoneyThomas <roneythomas6@gmail.com>
2016-12-13 13:50:10 -0500

    Added backend rendering feature

---
+++ restify/server.js
@@ -0,0 +1,118 @@
+let admin = require("firebase-admin");
+let cheerio = require('cheerio');
+let client = require("firebase-tools");
+let fs = require("fs");
+let path = require("path");
+let logging = require(path.resolve(__dirname, './utils/logging.js'));
+let restify = require('restify');
+
+let gcs;
+if (process.env.NODE_ENV === 'production') {
+    gcs = require('@google-cloud/storage');
+} else {
+    gcs = require('@google-cloud/storage')({
+        projectId: 'church-tools',
+        keyFilename: path.resolve(__dirname, './key.json')
+    });
+}
+let secrets = gcs.bucket('church-tools.appspot.com');
+let deployToken, serviceAccount;
+secrets.file('secret/church-tools-key.json').download((err, contents) => {
+    if (!err) {
+        // logging.info("index.js ", "deploy-token.json -- Success");
+        // logging.info("index.js church-tools-key.json ", contents.toString());
+        serviceAccount = JSON.parse(contents.toString());
+        console.log("sucedkssf key", serviceAccount);
+        // logging.writeLog("roneyoney", function (err, apiResponse) {
+        //     console.log(err, apiResponse);
+        // });
+        //logging.log.info(this.entry("seems like working"), this.handler);
+        logging.log.alert(logging.entry("lksdjksdf"), logging.handler);
+    } else {
+        //logging.error("index.js ", "deploy-token.json -- Error ", err);
+        // logging.writeLog("test log", "roney roney", function (err, apiResponse) {
+        //     console.log(apiResponse);
+        // });
+    }
+});
+secrets.file('secret/deploy-token.json').download((err, contents) => {
+    if (!err) {
+        // logging.info("app.js ", "deploy-token.json -- Success");
+        // logging.info("app.js deploy-token.json ", contents.toString());
+        deployToken = JSON.parse(contents.toString());
+        console.log("sucedkssf deploy token", deployToken);
+    } else {
+        // logging.error("index.js ", "deploy-token.json -- Error ", err);
+    }
+});
+let config;
+if (process.env.NODE_ENV === 'production') {
+    config = {
+        credential: admin.credential.applicationDefault(),
+        databaseURL: "https://church-tools.firebaseio.com/"
+    };
+} else {
+    serviceAccount = require("./key.json");
+    config = {
+        credential: admin.credential.cert(serviceAccount),
+        databaseURL: "https://church-tools.firebaseio.com/"
+    };
+}
+
+// Initialize the default app & db
+admin.initializeApp(config);
+let db = admin.database();
+
+function respond(req, res, next) {
+    if (req.params.ref === 'schedule') {
+        let ref = db.ref('schedule');
+        fs.readFile(path.resolve(__dirname, "./views/index.html"), "utf8", (err, data) => {
+            if (err) throw err;
+            $ = cheerio.load(data);
+            let scheduleElement = $('#upcoming_events');
+            ref.once("value", function (snapshot) {
+                snapshot.forEach(function (childSnapshot) {
+                    console.log(childSnapshot);
+                    console.log(childSnapshot.val().title);
+                    let title = "<b>" + childSnapshot.val().title + "</b><br>";
+                    scheduleElement.append(title);
+                    for (let x = 0, y = childSnapshot.val().events.length; x < y; x++) {
+                        console.log(childSnapshot.val().times[x] + ":", childSnapshot.val().events[x]);
+                        let event = childSnapshot.val().times[x] + ": " + childSnapshot.val().events[x] + "<br>";
+                        scheduleElement.append(event);
+                    }
+                });
+                res.end($.html());
+            });
+            fs.writeFile(path.resolve(__dirname, "./deploy/hosting/index.html"), $.html(), (err) => {
+                if (err) throw err;
+                console.log("It\"s saved!");
+                client.deploy({
+                    project: "church-tools",
+                    token: deployToken.token,
+                    cwd: path.resolve(__dirname, "../deploy/")
+                }).then(() => {
+                    logging.info("index.js ", "Firebase Hosting Deployed -- Success");
+                }).catch((err) => {
+                    // handle error
+                    logging.error("index.js ", "Firebase Hosting Deployed -- Error " + err);
+                });
+            });
+        });
+    } else {
+        return next(new restify.InvalidArgumentError("Invalid ref"));
+    }
+    next();
+}
+
+let server = restify.createServer({name: 'church-api'});
+server.get('/render/:ref', respond);
+server.head('/render/:ref', respond);
+
+server.listen(8080, function () {
+    console.log('%s listening at %s', server.name, server.url);
+});
+
+server.on('uncaughtException', function (req, res, route, err) {
+    console.log('uncaughtException', err.stack);
+});
---
+++ restify/utils/logging.js
@@ -0,0 +1,63 @@
+let logging;
+let restify = require('restify');
+let path = require("path");
+
+if (process.env.NODE_ENV === 'production') {
+    logging = require('@google-cloud/logging')();
+} else {
+    logging = require('@google-cloud/logging')({
+        projectId: 'church-tools',
+        keyFilename: path.resolve(__dirname, '../key.json')
+    });
+}
+
+let log = logging.log('restify');
+let client = restify.createJSONClient({
+    headers: {'Metadata-Flavor': 'Google'},
+    url: 'http://metadata.google.internal/computeMetadata/v1'
+});
+let metadata;
+if (process.env.NODE_ENV === 'production') {
+    client.get('/project/?recursive=true', (err, req, res, obj) => {
+        if (!err) {
+            console.log(err);
+        }
+        console.log('%j', obj);
+    });
+
+    client.get('/instance/?recursive=true', (err, req, res, obj) => {
+        if (!err) {
+            console.log(err);
+        }
+        console.log('%j', obj);
+        metadata = obj;
+    });
+} else {
+    metadata = {
+        resource: {
+            type: 'gce_instance',
+            labels: {
+                zone: 'global',
+                instance_id: '3'
+            }
+        }
+    };
+}
+
+function entry(data) {
+    return log.entry(metadata, data);
+}
+
+function handler(err, apiResponse) {
+    if (!err) {
+        console.log("working")
+    } else {
+        console.log("not wokring");
+    }
+}
+
+module.exports = {
+    entry: entry,
+    log: log,
+    handler: handler
+};

6daab57a90e3be0b20fc87f6d7bcc557b46f9367
RoneyThomas <roneythomas6@gmail.com>
2017-02-08 04:21:07 -0500

    Now saves donors to firebase

--- backend/church-tools/routes/index.js
+++ backend/church-tools/routes/index.js
@@ -12,10 +12,45 @@ router.get('/donors', function (req, res, next) {
         console.log("Seems like working");
         res.type('json');
         res.json({
-            "title": "Page Title",
+            "title": "Donors",
             "body": {
-                "page-content":"<h1>Yes Yes</h1>"
-            }
+                "page-content":`<script>
+firebase.database.enableLogging(true);
+function submitFirebase() {
+    var myForm = document.getElementById("donorForm");
+  for (var i = 0; i < myForm.elements.length; i++) {
+    console.log(myForm.elements[i].value);
+    }
+    var donorRef = firebase.database().ref('donors').push();
+    donorRef.set({
+        'name' : myForm.elements[0].value,
+    'email' : myForm.elements[1].value,
+    'phoneNo' : myForm.elements[2].value
+    });
+}</script>
+<h3>Add Donor</h3><div class="mdl-grid">
+                <form id="donorForm">
+  <div class="mdl-textfield mdl-js-textfield">
+    <input class="mdl-textfield__input" type="text" id="name">
+    <label class="mdl-textfield__label" for="sample1">Name</label>
+  </div>
+  <div class="mdl-textfield mdl-js-textfield">
+    <input class="mdl-textfield__input" type="email" id="email">
+    <label class="mdl-textfield__label" for="sample1">donor@gmail.com</label>
+  </div>
+  <div class="mdl-textfield mdl-js-textfield">
+  <!--Fill the pattern for phone n.o-->
+    <input class="mdl-textfield__input" type="text" pattern="" id="phoneNo">
+    <label class="mdl-textfield__label" for="sample2">Number...</label>
+    <span class="mdl-textfield__error">Input is not a number!</span>
+  </div>
+</form>
+<button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect" onclick="submitFirebase()">
+  Submit
+</button>
+                </div>`
+            },
+            "foot": "<script>componentHandler.upgradeDom();</script>"
         });
     } else {
         // Send the whole page

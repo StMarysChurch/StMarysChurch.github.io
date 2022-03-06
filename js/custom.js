88c9baf1428a4caebe31e6edb4beb735ad867844
RoneyThomas <roneythomas6@gmail.com>
2022-03-06 01:50:32 -0500

    Remove unused tags and JS

--- js/custom.js
+++
@@ -1,8 +0,0 @@
-// Target specific elements
-$(function(){
-	$("li:first-child").addClass("first");
-	$("li:last-child").addClass("last");
-	$("div.widget:first").addClass("first");
-	$("div.widget:last").addClass("last");
-	$(".navbar ul li ul li:has(ul)").find("a:first").append(" &raquo;");
-});
